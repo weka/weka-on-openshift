f08bf8925846c92800ccaa74d84ee6530cdab71d
Bala Ramesh <bala.ramesh@weka.io>
2026-03-31 10:08:37 -0500

    Obtain Linux aarch64 binaries for openshift

--- deploy-ocp.sh
+++ deploy-ocp.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ##This script creates an OpenShift cluster with ARM instances for you##
-##Best used on a free AWS Graviton bastion host or on a mac with ARM chip###
+##Best used on a free AWS Graviton bastion host###
 
 #Adjust these variables
 # set aws access variables
@@ -13,17 +13,16 @@ export OCP_VERSION=4.19.27
 clear
 
 echo "#.#.#.#.Begin run to deploy an OpenShift cluster of version ${OCP_VERSION} on ARM instances.#.#.#.#"
-echo "...\n...\n"
 
-echo "Dowloading and extracting Openshift installer version ${OCP_VERSION}..."
-command wget -nc https://mirror.openshift.com/pub/openshift-v4/aarch64/clients/ocp/${OCP_VERSION}/openshift-install-mac.tar.gz
-command tar xzvf openshift-install-mac.tar.gz openshift-install
+echo "Dowloading and extracting Openshift installer version ${OCP_VERSION} for Linux aarch64..."
+command wget -nc https://mirror.openshift.com/pub/openshift-v4/aarch64/clients/ocp/${OCP_VERSION}/openshift-install-linux.tar.gz
+command tar xzvf openshift-install-linux.tar.gz openshift-install
 command chmod u+x openshift-install
 command cp openshift-install ~/bin/
 
-echo "Downloading oc client version ${OCP_VERSION}.."
-command wget -nc https://mirror.openshift.com/pub/openshift-v4/x86_64/clients/ocp/${OCP_VERSION}/openshift-client-mac.tar.gz
-command tar xzvf openshift-client-mac.tar.gz oc
+echo "Downloading oc client version ${OCP_VERSION} for Linux aarch64..."
+command wget -nc https://mirror.openshift.com/pub/openshift-v4/aarch64/clients/ocp/${OCP_VERSION}/openshift-client-linux.tar.gz
+command tar xzvf openshift-client-linux.tar.gz oc
 command chmod u+x oc
 command cp oc ~/bin/
 
